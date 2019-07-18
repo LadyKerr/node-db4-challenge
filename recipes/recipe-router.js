609df92f96d650582b918022c410fc399b71cd75
LadyKerr <kadiekerr@gmail.com>
2019-07-18 18:30:28 -0400

    server & recipe-model

---
+++ recipes/recipe-router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+// const Recipes = require("./recipe-model");
+
+const router = express.Router();
+
+//GET all recipes in db
+
+//GET ingredients & quantities for recipe by id
+
+//GET steps for recipe id
+
+module.exports = router;
